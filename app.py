c5fcca2b3cca3983e8aed238611d2bb0a77a9bd8
miho777 <naozumi.miho@emc.com>
2020-11-06 01:31:09 +0900

    blue deploy

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return '<h1 style="color:green">Hello Piper!</h1>'
+    return '<h1 style="color:blue">Hello Piper!</h1>'
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=int(os.getenv('PORT', '5000')))
+    app.run(debug=False, host='0.0.0.0', port=int(os.getenv('PORT', '5000')), threaded=True)
